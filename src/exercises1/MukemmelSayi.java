87e71d6f47e411b556b7058b850b3a4846fa16b2
Gülbeyaz <Glbeyaz@172.16.16.221>
2022-09-26 15:09:07 +0300

    Editing

--- src/exercises1/MukemmelSayi.java
+++ src/exercises1/MukemmelSayi.java
@@ -3,7 +3,21 @@ package exercises1;
 public class MukemmelSayi {
 
 	public static void main(String[] args) {
+		//6-->1,2,3-->1+2+3=6
 		
+		int number=5;
+		int toplamSayi=0;
+		for(int i=1; i<number; i++)  {
+			if(number % i==0) {
+				toplamSayi=toplamSayi+i;
+			}
+			
+		}
+		if (toplamSayi==number) {
+			System.out.println(number + " " + "Mükemmel Sayidir");
+		}else {
+			System.out.println(number + " " + "Mükemmel Sayi Değildir");
+		}
 	}
 
 }
