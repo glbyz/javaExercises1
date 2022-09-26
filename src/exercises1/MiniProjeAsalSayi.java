08ee5866c842bf800f83783818ed1b6f368128b9
Gülbeyaz <Glbeyaz@172.16.16.221>
2022-09-26 12:21:48 +0300

    editing

--- src/exercises1/MiniProjeAsalSayi.java
+++ src/exercises1/MiniProjeAsalSayi.java
@@ -2,11 +2,29 @@ package exercises1;
 
 public class MiniProjeAsalSayi {
 
-	public static void main(String[] args) {
-		int number = 25;
-		int remainder = number %2;
-		System.out.println(remainder);
 
-	}
+		public static void main(String[] args) {
+
+			
+			int number = 8;
+			int j = 0;
+			for(double i = 1;i<=number;i++) {
+				for(int k=1;k<=number;k++) {
+					if(number/i==k) {
+						j++;
+					}
+				}
+		
+				
+			}
+			if(j<3) {
+				System.out.println(number+" sayısı asaldır.");
+			}
+			else {
+				System.out.println(number+" sayısı asal değildir.");
+			}
+
+		}
+
 
 }
